12d597c5382c7a124a2c57c05e784b342d0c714e
Pratik Gawand <gawand.p@northeastern.edu>
2022-02-22 20:27:01 -0500

    Added migration for investor funds

---
+++ migrations/sqls/20220223011901-add-investor-wallet-table-up.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS investor_funds;
+CREATE TABLE investor_funds (
+ trasactionId UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+ debit_amount REAL NOT NULL ,
+ credit_amount REAL NOT NULL,
+ description TEXT,
+ user_id UUID NOT NULL,
+ trasactionDate timestamptz NOT NULL DEFAULT now() ,
+ created_at timestamptz NOT NULL DEFAULT now(),
+ updated_at timestamptz NOT NULL DEFAULT now(),
+ CONSTRAINT fk_users_investor_funds FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
